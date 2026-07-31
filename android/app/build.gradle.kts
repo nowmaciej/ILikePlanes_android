7e9c3983a205d5e8b101ee0d3226296267dd253f
nowmaciej <now.maciej@gmail.com>
2026-07-31 13:23:05 +0200

    Gradle update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.ilikeplanes.app"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.ilikeplanes.app"
         minSdk = 29
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0.0"
 
